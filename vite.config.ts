ad681e2d19d490c03ee75d1ff8d9db2fe58fe7e3
kayibea <gatse1531@gmail.com>
2025-07-21 15:50:34 +0200

    🔥 Remove unused code

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vite.dev/config/
 export default defineConfig({
   build: { emptyOutDir: true },
-  // build: {
-  //   outDir: "dist",
-  //   emptyOutDir: true,
-  //   rollupOptions: {
-  //     input: {
-  //       main: "src/main.ts",
-  //     },
-  //     output: {
-  //       entryFileNames: "scripts/[name].js",
-  //       chunkFileNames: "scripts/[name].js",
-  //       assetFileNames: "assets/[name][extname]",
-  //       manualChunks: undefined,
-  //     },
-  //   },
-  // },
   plugins: [svelte()],
 });
