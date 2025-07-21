cb1bf98002a6c4f054d9e83d53ed3abfb34ba3c1
kayibea <gatse1531@gmail.com>
2025-07-21 19:32:38 +0200

    🔧 Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vite.dev/config/
 export default defineConfig({
   build: { emptyOutDir: true },
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      utils: path.resolve('src', 'utils'),
+      constant: path.resolve('src', 'constant'),
+    },
+  },
 });
