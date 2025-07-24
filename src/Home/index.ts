0855e04b434aa9550f848f2617fc4775b1089e60
kayibea <gatse1531@gmail.com>
2025-07-24 06:15:50 +0200

    ✨ Add bulk compoment export

---
+++ src/Home/index.ts
@@ -0,0 +1,10 @@
+import Matrix from './Matrix.svelte';
+import Constelation from './Constelation.svelte';
+import Constelation2 from './Constelation2.svelte';
+
+export const backgrounds = [
+  Matrix,
+  Constelation,
+  Constelation2,
+  //
+];
