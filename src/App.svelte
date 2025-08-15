7b9bf813e440eea9e15da2362ae5c6518b04773d
kayibea <gatse1531@gmail.com>
2025-08-15 19:39:12 +0200

    test: hard code matrix background animation into the homepage

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { backgrounds } from 'Home';
-  import Constelation from 'Home/Constelation.svelte';
+  import { backgrounds } from 'Components';
+  // import Constelation from 'Home/Constelation.svelte';
+  import Matrix from 'Components/Matrix.svelte';
 
   let [width, height] = $state([window.innerWidth, window.innerHeight]);
   const abortController = new AbortController();
@@ -20,7 +21,8 @@
 </script>
 
 <main>
-  <Background {width} {height} />
+  <Matrix {width} {height} />
+  <!-- <Background {width} {height} /> -->
   <!-- <Constelation {width} {height} /> -->
   <!-- <img src="assets/img/nebula.jpg" alt="" /> -->
 </main>
