5bb07b445b83ad4f9f1e051d464fbace2e5d0287
kayibea <gatse1531@gmail.com>
2025-08-15 02:35:01 +0200

    style: change constelation canvas background to black

--- src/Home/Constelation.svelte
+++ src/Home/Constelation.svelte
@@ -138,10 +138,6 @@
 <style>
   canvas {
     display: block;
-    /* background: #000; */
-    /* background-image: url(assets/img/nebula.jpg); */
-    background-image: url(assets/img/nebula.jpg);
-    background-size: cover;
-    /* background-repeat: no-repeat; */
+    background-color: black;
   }
 </style>
