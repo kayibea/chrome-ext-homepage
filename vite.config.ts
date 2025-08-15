98967205892b21931f2347751318a92869f934c0
kayibea <gatse1531@gmail.com>
2025-08-16 00:31:35 +0200

    ci: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   resolve: {
     alias: {
+      animations: path.resolve('src', 'animations'),
+      types: path.resolve('src', 'types'),
       constant: path.resolve('src', 'constant'),
     },
   },
