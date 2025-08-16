76ccd87d0127fa6cf096470ea7642506aaa9b5a1
kayibea <gatse1531@gmail.com>
2025-08-16 03:08:19 +0200

    remove: delete Animation2D class

--- src/animations/Animation2D.ts
+++
@@ -1,6 +0,0 @@
-export type Animation2DConstructor = new (...args: any[]) => Animation2D;
-
-export default abstract class Animation2D {
-  public abstract draw(ctx: CanvasRenderingContext2D): void;
-  public setupListeners(): void {}
-}
