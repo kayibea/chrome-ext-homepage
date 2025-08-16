790114d3a2ce64d3cf752ba078af6fa0afb715c0
kayibea <gatse1531@gmail.com>
2025-08-16 03:11:24 +0200

    refactor: append custom animated canvas to the DOM

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,4 @@
 import './style.css';
-import animation from 'animations';
+import canvas from 'animations';
 
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const ctx = canvas.getContext('2d')!;
-const fpsInterval = 1000 / 60; // 60fps cap
-
-requestAnimationFrame(animate);
-
-let lastTime = 0;
-function animate(timestamp: number): void {
-  if (timestamp - lastTime >= fpsInterval) {
-    lastTime = timestamp;
-    animation.draw(ctx);
-  }
-  requestAnimationFrame(animate);
-}
+document.body.appendChild(canvas);
