a9363ce75fddc3713262dd017a8cb40614f10b0d
kayibea <gatse1531@gmail.com>
2025-08-16 03:17:16 +0200

    refactor: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   resolve: {
     alias: {
-      animations: path.resolve('src', 'animations'),
       types: path.resolve('src', 'types'),
       constant: path.resolve('src', 'constant'),
+      animations: path.resolve('src', 'animations'),
     },
   },
 });
